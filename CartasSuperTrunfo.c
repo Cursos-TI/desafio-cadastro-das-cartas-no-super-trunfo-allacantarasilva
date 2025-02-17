2ad6669f46462af4b31a137368a9a1e524b367c2
allacantarasilva <allcantarasilva.estacio@gmail.com>
2025-02-17 22:38:03 +0000

    Construcao1702

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,43 @@
 
 int main(){
 
-    printf("Desafio super trunfo");
+    char nome_cidade[50];
+    int populacao, codigo;
+    float area;
+    
+
+
+    printf("Desafio Super Trunfo\n");
+    printf("Vamos cadastrar as primeiras cartas?: \n");
+
+    printf("Digite o código da cidade: \n");
+    scanf("%d", &codigo);
+
+    printf("Digite por favor o nome da cidade: \n");
+    scanf(" %s", nome_cidade);
+
+    printf("Digite a população: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite a area total em m²: \n");
+    scanf("%f", &area);
+
+    printf(">>Cidade: %d\n ", codigo);
+    printf(">>Nome da cidade: %s\n", nome_cidade);
+    printf(">>População: %d\n", populacao);
+    print("Area total em m²\n");
+
+
+
+    printf("Fim...\n");
+
+    
+
+
+
+
+
+
 
 
 
